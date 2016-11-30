2629376974003d6b654c141316208873702c0da7
Sugosh Ravindra <sugosh.nr@gmail.com>
2016-11-30 17:20:30 -0500

    Connectinh wit.ai and completing the flow

---
+++ pingherapp/Converse.py
@@ -0,0 +1,19 @@
+from wit import Wit
+
+
+def converse(Query):
+	access_token = "7MI6KCEAOV24GWYB7TD7RALBDXMANCB3"
+
+
+	def send(request, response):
+	    print(response['text'])
+	actions = {
+	    'send': send,    
+	}
+
+	client = Wit(access_token=access_token, actions=actions)
+	data=client.converse(1,Query)
+	X=[]
+	for key in data['entities'].keys():
+	   	X.append(data['entities'][key][0]['value'])
+	return X
--- pingherapp/views.py
+++ pingherapp/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+import Converse
 
 def index(request):
     template = loader.get_template('pingherapp/iq/index.html')
     context = {
         'latest_question_list': 'testMohit',
     }
+    temp1=Converse.converse("Who won the 2016 elections")
+	temp2=Converse.converse("who won the elections")
+	print temp1
+	print temp2
+    return HttpResponse(template.render(context, request))
