6575528dd2c28c14416c4e0134d745e5f895b68e
Sugosh Ravindra <sugosh.nr@gmail.com>
2016-12-03 01:47:45 -0500

    Default questions in mydict.json updated witaihandler and solrhandler

--- pingherapp/solrhandler.py
+++ pingherapp/solrhandler.py
@@ -4,7 +4,7 @@ import json
 def solrcall(string):
 	
 	#Getting SOLR Hosted URL and docs
-	inurl = "http://54.212.247.174:8983/solr/#/pingher_nort/select?q="+urllib2.quote(string)+"&wt=json"
+	inurl = "http://54.212.247.174:8983/solr/pingher_nort/select?q="+urllib2.quote(string)+"&wt=json"
 	data = urllib2.urlopen(inurl)
 	docs = json.load(data)['response']['docs']
 	print string
--- pingherapp/witaihandler.py
+++ pingherapp/witaihandler.py
@@ -1,5 +1,6 @@
 import solrhandler
 from wit import Wit
+import json
 
 def converse(Query):
 	access_token = "HUYFSZATE2FRGLETGVWWTCHNSVTXBKDC" 
@@ -12,9 +13,25 @@ def converse(Query):
 	}
 
 	client = Wit(access_token=access_token, actions=actions)
+	#Query=""
 	data=client.converse(1,Query)
 	X=""
+	tweet=""
 	for key in data['entities'].keys():
 	   	X=X+" "+(data['entities'][key][0]['value'])
-	tweet=solrhandler.solrcall(X)
+	mydict=json.load(open("mydict.json"))
+	solr=True
+	for key_list in mydict.keys():
+		flag=True
+		for key in data['entities'].keys():
+			if str(data['entities'][key][0]['value']).upper() not in str(key_list):
+				flag=False
+		if flag:
+			tweet=mydict[key_list]
+			solr=False
+
+	if solr:
+		tweet=solrhandler.solrcall(X)
+	
 	return tweet
+
