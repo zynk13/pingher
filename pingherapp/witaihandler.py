89210f198a515f63911ae51a19231c3538fad669
Sugosh Ravindra <sugosh.nr@gmail.com>
2016-12-04 03:44:56 -0500

    what's your name? default

--- pingherapp/witaihandler.py
+++ pingherapp/witaihandler.py
@@ -26,9 +26,11 @@ def converse(Query):
 	for key_list in mydict.keys():
 		flag=True
 		for key in data['entities'].keys():
+			print data['entities'][key][0]['value'],key_list
 			if str(data['entities'][key][0]['value']).upper() not in str(key_list):
 				flag=False
 		if flag:
+			print data['entities'][key][0]['value'],key_list
 			tweet['tweet_text']=mydict[key_list]
 			solr=False
 	if len(data['entities'].keys())==0:
@@ -36,5 +38,7 @@ def converse(Query):
 		X=Query
 	if solr:
 		tweet=solrhandler.solrcall(X,data)
+	print tweet
+	print X
 	return tweet
 
