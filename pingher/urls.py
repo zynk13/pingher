f2c258ff3712d157677528c28f17787c604a55be
Mohit-Ak <mohitkhakharia@gmail.com>
2016-12-05 01:56:58 -0500

    Url directing only to Pingher

--- pingher/urls.py
+++ pingher/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^pingher/', include('pingherapp.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'', include('pingherapp.urls')),
+    #url(r'^admin/', admin.site.urls),
 ]
